a1b5d03cbb50d389f59ebb2068a0ac498e44ffa7
Will Wang <willy.j.wang@gmail,com>
2022-08-03 15:20:24 -0500

    Add locations

--- friends.rb
+++ friends.rb
@@ -2,3 +2,9 @@ arrays = ["Bill", "James", "Garfield", "Will", "Sam"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+
+# Locations
+locations = ["Asia", "North America", "Australia", "Europe", "South America"]
+locations.unshift("Antarctica")
+puts locations
